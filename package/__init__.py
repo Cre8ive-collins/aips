3845e80d7315495cb2c4f94f8eb238cc4566da3a
Cre8ive-collins <wilsoncollins44@yahoo.com>
2020-06-02 13:00:32 +0100

    verion 1 production ready

--- package/__init__.py
+++ package/__init__.py
@@ -9,6 +9,9 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '78babfa8647ec7b0d1ba50a8d1d00c7f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
